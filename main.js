e8229265c2c195e3d72b332f5b1e5b4c1a06e576
unknown <tomasz.czechlewski@gmail.com>
2022-01-23 23:24:41 +0100

    added other way to code it in JS

--- main.js
+++ main.js
@@ -2,28 +2,46 @@ const btnMenu = document.querySelector('button')
 const timesIcon = document.querySelector('.fa-times')
 const barsIcon = document.querySelector('.fa-bars')
 const navLinks = document.querySelectorAll('li')
-const ulList = document.querySelector('ul')
+const ulList = document.querySelector('nav ul')
 
 
-// this function is responsible for changing the menu icons 
-const showHide = () => {
-	if (timesIcon.classList.contains('hide')) {
-		timesIcon.classList.remove('hide')
-		barsIcon.classList.add('hide')
-        ulList.classList.remove('hide')
-	} else {
-		barsIcon.classList.remove('hide')
-		timesIcon.classList.add('hide')
-        ulList.classList.add('hide')
-	}
+//= this function is responsible for changing the menu icons 
+
+// one way of doing this 
+// const showHide = () => {
+// 	if (timesIcon.classList.contains('hide')) {
+// 		timesIcon.classList.remove('hide')
+// 		barsIcon.classList.add('hide')
+//         ulList.classList.remove('hide')
+// 	} else {
+// 		barsIcon.classList.remove('hide')
+// 		timesIcon.classList.add('hide')
+//         ulList.classList.add('hide')
+// 	}
+// }
+
+// const hideAfterClick = () => {
+//     ulList.classList.add('hide')
+//     barsIcon.classList.remove('hide')
+//     timesIcon.classList.add('hide')
+
+// // }
+// btnMenu.addEventListener('click', showHide)
+// navLinks.forEach(nav => nav.addEventListener('click', hideAfterClick))
+
+// second way of doing this 
+
+const showHide2 = () => {
+    ulList.classList.toggle('hide')
+    timesIcon.classList.toggle('hide')
+    barsIcon.classList.toggle('hide')
+
+
 }
-
 const hideAfterClick = () => {
-    ulList.classList.add('hide')
-    barsIcon.classList.remove('hide')
-    timesIcon.classList.add('hide')
-
+        ulList.classList.add('hide')
+        barsIcon.classList.remove('hide')
+        timesIcon.classList.add('hide')
 }
-
-btnMenu.addEventListener('click', showHide)
 navLinks.forEach(nav => nav.addEventListener('click', hideAfterClick))
+btnMenu.addEventListener('click', showHide2)
